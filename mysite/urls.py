b8aab3e8ed1a4f3d46c43b3afc710d5a332adfa0
Prabhu <mprabhu.in@gmail.com>
2018-09-10 14:47:02 -0700

    Personal Website detailsv1

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls')),
+   url(r'^$', include('personal.urls')),
 ]
